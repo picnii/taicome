a96534dd1836943830446b99ee5732744b3ddd1a
Phisuthisak Ngernkuakul <phisuthisakn@gmail.com>
2013-09-15 14:45:20 +0700

    change level

--- js/Controller.js
+++ js/Controller.js
@@ -1,19 +1,23 @@
 function HomeCtrl($scope, $rootScope, Question)
 {
 	$scope.level = loadCurrentLevel();
-	$scope.question = Question.get({level:$scope.level}, function(data){
-
-		console.log(data)
-	});
+	$scope.question = Question.get({level:$scope.level});
 
 	$scope.checkAnswer = function()
 	{
 		console.log($scope.answer);
+		console.log($scope.question.answer);
+
+		if($scope.question.answer == $scope.answer)
+		{
+			$scope.getNewQuestion();
+		}
 	}
 
 	$scope.getNewQuestion = function()
 	{
-
+		$scope.level++;
+		console.log($scope.level)
 	}
 
 	$scope.getHint = function()
