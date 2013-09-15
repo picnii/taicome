e14266d30ba6a14443a48e31f671cef13aff77ed
picnii <sompop@picnii.me>
2013-09-15 20:25:01 +0700

    add win page

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@ var app = angular.module('taicome', ['dataServices']).
 		  config(['$routeProvider', function($routeProvider) {
 		  $routeProvider.
 		  	  when('/', {templateUrl: 'template/home.html',   controller: HomeCtrl}).
+                 when('/win', {templateUrl:'template/win.html', controller:WinCtrl}).
 		      otherwise({redirectTo: '/'});
 		}]);
