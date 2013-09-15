18d439ea5c92fcecf2490c061390b0afffba4dd5
Phisuthisak Ngernkuakul <phisuthisakn@gmail.com>
2013-09-15 17:05:57 +0700

    get new level and save level

--- js/init.js
+++ js/init.js
@@ -6,5 +6,18 @@ app.run(function($rootScope) {
 
 function loadCurrentLevel()
 {
-	return 1;
+if (localStorage.lastplayedlevel)
+  {
+  localStorage.lastplayedlevel=Number(localStorage.lastplayedlevel);
+  }
+else
+  {
+  localStorage.lastplayedlevel=1;
+  }
+	return localStorage.lastplayedlevel;
+}
+
+function saveLevel(level)
+{
+	localStorage.lastplayedlevel = level;
 }
