317cf6e73dc54c5549469334f86130a569129c4b
picnii <sompop@picnii.me>
2013-09-15 14:45:19 +0700

    wrong

--- js/Controller.js
+++ js/Controller.js
@@ -1,14 +1,14 @@
 function HomeCtrl($scope, $rootScope, Question)
 {
 	$scope.level = loadCurrentLevel();
-	$scope.question = Question.get({level:$scope.level}, function(data){
+	$scope.question = Question.get({level:2}, function(data){
 
 		console.log(data)
 	});
 
 	$scope.checkAnswer = function()
 	{
-
+		console.log($scope.question.answer)
 	}
 
 	$scope.getNewQuestion = function()
